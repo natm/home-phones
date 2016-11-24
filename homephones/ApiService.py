c40996a139ae3ab12f875c6f3ca62f23656c6614
Nat Morris <nat@nuqe.net>
2016-11-24 23:46:38 +0000

    inbound calls routed to a single phone, outbound numbers not parsed yet

--- homephones/ApiService.py
+++ homephones/ApiService.py
@@ -9,30 +9,15 @@ from flask import Flask, redirect, url_for, Response, request
 
 LOG = logging.getLogger(__name__)
 
+def parse_dialed_number(number):
+    fullnumber = "+447531750292"
+    describednumber = "Test"
+    return (fullnumber, describednumber)
+
 def twiml(resp):
     resp = Response(str(resp))
     resp.headers['Content-Type'] = 'text/xml'
     return resp
-
-def _give_instructions(response):
-    response.say("To get to your extraction point, get on your bike and go down " +
-                 "the street. Then Left down an alley. Avoid the police cars. Turn left " +
-                 "into an unfinished housing development. Fly over the roadblock. Go " +
-                 "passed the moon. Soon after you will see your mother ship.",
-                 voice="alice", language="en-GB")
-
-    response.say("Thank you for calling the ET Phone Home Service - the " +
-                 "adventurous alien's first choice in intergalactic travel")
-
-    response.hangup()
-    return response
-
-def _list_planets(response):
-    with response.gather(numDigits=1, action=url_for('planets'), method="POST") as g:
-        g.say("To call the planet Broh doe As O G, press 2. To call the planet " +
-              "DuhGo bah, press 3. To call an oober asteroid to your location, press 4. To " +
-              "go back to the main menu, press the star key ",
-              voice="alice", language="en-GB", loop=3)
 
 class ApiService(object):
 
@@ -52,39 +37,19 @@ class ApiService(object):
     @staticmethod
     @app.route("/v1/from_external/", methods=['POST'])
     def from_external():
+        caller = str(request.form["Caller"]).replace("client:", "")
         response = twilio.twiml.Response()
-        with response.gather(numDigits=1, action=url_for('menu'), method="POST") as g:
-            g.play(url="http://howtodocs.s3.amazonaws.com/et-phone.mp3", loop=3)
+        response.say("Thank you for calling Claire and Nat, please hold.", voice="alice", language="en-GB")
+        response.dial(callerId=caller).sip("officephone1@esgob.sip.us1.twilio.com")
         return twiml(response)
 
     @staticmethod
-    @app.route('/ivr/menu', methods=['POST'])
-    def menu():
-        selected_option = request.form['Digits']
-        option_actions = {'1': _give_instructions,
-                          '2': _list_planets}
-
-        if option_actions.has_key(selected_option):
-            response = twilio.twiml.Response()
-            option_actions[selected_option](response)
-            return twiml(response)
-
-        return _redirect_welcome()
-
-    @staticmethod
-    @app.route('/ivr/planets', methods=['POST'])
-    def planets():
-        selected_option = request.form['Digits']
-        option_actions = {'2': "+12024173378",
-                          '3': "+12027336386",
-                          "4": "+12027336637"}
-
-        if option_actions.has_key(selected_option):
-            response = twilio.twiml.Response()
-            response.dial(option_actions[selected_option])
-            return twiml(response)
-
-        return _redirect_welcome()
-
-
-    # private methods
+    @app.route("/v1/from_internal/", methods=['POST'])
+    def from_internal():
+        called = str(request.form["Called"])
+        number = called.split("@")[0].split("sip:")[1]
+        (fullnumber, describednumber) = parse_dialed_number(number=number)
+        response = twilio.twiml.Response()
+        response.say("Calling %s" % (describednumber), voice="alice", language="en-GB")
+        response.dial(number=fullnumber, callerId="+441437766027")
+        return twiml(response)
