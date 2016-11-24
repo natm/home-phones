2e53c26c08df2e00a7d755cf045327496ae345a4
Nat Morris <nat@nuqe.net>
2016-11-24 22:45:44 +0000

    dummy web app working

--- homephones/ApiService.py
+++ homephones/ApiService.py
@@ -4,7 +4,7 @@
 import logging
 import twilio.twiml
 
-from flask import Flask, redirect, url_for, Response
+from flask import Flask, redirect, url_for, Response, request
 
 
 LOG = logging.getLogger(__name__)
@@ -13,6 +13,26 @@ def twiml(resp):
     resp = Response(str(resp))
     resp.headers['Content-Type'] = 'text/xml'
     return resp
+
+def _give_instructions(response):
+    response.say("To get to your extraction point, get on your bike and go down " +
+                 "the street. Then Left down an alley. Avoid the police cars. Turn left " +
+                 "into an unfinished housing development. Fly over the roadblock. Go " +
+                 "passed the moon. Soon after you will see your mother ship.",
+                 voice="alice", language="en-GB")
+
+    response.say("Thank you for calling the ET Phone Home Service - the " +
+                 "adventurous alien's first choice in intergalactic travel")
+
+    response.hangup()
+    return response
+
+def _list_planets(response):
+    with response.gather(numDigits=1, action=url_for('planets'), method="POST") as g:
+        g.say("To call the planet Broh doe As O G, press 2. To call the planet " +
+              "DuhGo bah, press 3. To call an oober asteroid to your location, press 4. To " +
+              "go back to the main menu, press the star key ",
+              voice="alice", language="en-GB", loop=3)
 
 class ApiService(object):
 
@@ -37,6 +57,7 @@ class ApiService(object):
             g.play(url="http://howtodocs.s3.amazonaws.com/et-phone.mp3", loop=3)
         return twiml(response)
 
+    @staticmethod
     @app.route('/ivr/menu', methods=['POST'])
     def menu():
         selected_option = request.form['Digits']
@@ -50,7 +71,7 @@ class ApiService(object):
 
         return _redirect_welcome()
 
-
+    @staticmethod
     @app.route('/ivr/planets', methods=['POST'])
     def planets():
         selected_option = request.form['Digits']
@@ -67,24 +88,3 @@ class ApiService(object):
 
 
     # private methods
-
-    def _give_instructions(response):
-        response.say("To get to your extraction point, get on your bike and go down " +
-                     "the street. Then Left down an alley. Avoid the police cars. Turn left " +
-                     "into an unfinished housing development. Fly over the roadblock. Go " +
-                     "passed the moon. Soon after you will see your mother ship.",
-                     voice="alice", language="en-GB")
-
-        response.say("Thank you for calling the ET Phone Home Service - the " +
-                     "adventurous alien's first choice in intergalactic travel")
-
-        response.hangup()
-        return response
-
-
-    def _list_planets(response):
-        with response.gather(numDigits=1, action=url_for('planets'), method="POST") as g:
-            g.say("To call the planet Broh doe As O G, press 2. To call the planet " +
-                  "DuhGo bah, press 3. To call an oober asteroid to your location, press 4. To " +
-                  "go back to the main menu, press the star key ",
-                  voice="alice", language="en-GB", loop=3)
