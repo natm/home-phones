8f24d8d2cb7acd622cce088f30116d15a2cbd9bf
Nat Morris <nat@nuqe.net>
2016-11-27 22:03:09 +0000

    cleaned up v1 api

--- homephones/views/v1/api.py
+++ homephones/views/v1/api.py
@@ -4,7 +4,7 @@
 import logging
 import twilio.twiml
 
-from flask import Blueprint, Flask, redirect, url_for, Response, request
+from flask import Blueprint, Response, request
 from homephones.dialhelper import evalute_number
 
 LOG = logging.getLogger(__name__)
@@ -12,48 +12,32 @@ LOG = logging.getLogger(__name__)
 
 api = Blueprint('apiv1', __name__, url_prefix='/v1')
 
+
 def parse_dialed_number(number):
     fullnumber = evalute_number(dialed=number)
-    describednumber = "Test"
+    describednumber = ""
     return (fullnumber, describednumber)
+
 
 def twiml(resp):
     resp = Response(str(resp))
     resp.headers['Content-Type'] = 'text/xml'
     return resp
 
+
 @api.route("/")
 def index():
     return "Home phones v1", 200
 
+
 @api.route("/from_external/", methods=['POST'])
 def from_external():
     caller = str(request.form["Caller"]).replace("client:", "")
-    #response = twilio.twiml.Response()
-    #response.say("Thank you for calling Claire and Nat, please hold.", voice="alice", language="en-GB")
-    #response.dial(callerId=caller).sip("officephone1@esgob.sip.us1.twilio.com").sip("cordlessphone1@esgob.sip.us1.twilio.com")
-    #return twiml(response)
-
-    x = """<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-<Say language="en-GB" voice="alice">Please hold</Say>
-<Dial callerId="TEST" action="https://phones.gorras.hw.esgob.com/v1/call_action/">
-    <Sip>officephone1@esgob.sip.us1.twilio.com</Sip>
-</Dial>
-<Dial callerId="TEST" action="https://phones.gorras.hw.esgob.com/v1/call_action/">
-    <Sip>natimac1@esgob.sip.us1.twilio.com</Sip>
-</Dial>
-</Response>"""
-    #resp = Response(x)
-    #resp.headers['Content-Type'] = 'text/xml'
-    #return resp
     response = twilio.twiml.Response()
-    response.say("please hold")
-    response.enqueue(name="inbound")
+    response.say("Thank you for calling Claire and Nat, please hold.", voice="alice", language="en-GB")
+    response.dial(callerId=caller).sip("cordlessphone1@esgob.sip.us1.twilio.com")
     return twiml(response)
-    #response.say("please hold.", voice="alice", language="en-GB")
-    #response.enqueue(callerId=caller).sip("officephone1@esgob.sip.us1.twilio.com").sip("cordlessphone1@esgob.sip.us1.twilio.com")
-    #return twiml(response)
+
 
 @api.route("/from_internal/", methods=['POST'])
 def from_internal():
@@ -65,13 +49,6 @@ def from_internal():
     response.dial(number=fullnumber, callerId="+441437766027")
     return twiml(response)
 
-@api.route("/call_action/", methods=['POST'])
-def call_action():
-    resp = Response("""<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-</Response>""")
-    resp.headers['Content-Type'] = 'text/xml'
-    return resp
 
 @api.route("/status_callback/", methods=['POST'])
 def status_callback():
